c19dddec308fa0cdabe70c63e308d3e30d46db7d
gkcaday13 <158866408+gkcaday13@users.noreply.github.com>
2024-02-21 10:35:32 +0800

    Django

---
+++ Mission.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("The College of Computing and Multimedia Studies shall produce competent and innovative professionals or Technopreneurs in the Information and Communication Technology (ICT) industry adequately prepared in the practice of their profession supportive of national development goals and standards of global excellence.")
